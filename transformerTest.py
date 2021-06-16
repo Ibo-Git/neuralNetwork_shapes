bf3b9e33a4e592f2ec4e97193bd2a9d2cba9e563
Ibo-Git <ibo1997.it+git@gmail.com>
2021-06-16 22:53:28 +0200

    added training and finished model

--- transformerTest.py
+++ transformerTest.py
@@ -35,13 +35,36 @@ class modelTransformer(nn.Module):
     def __init__(self, src_vocab_size, embedding_size, trg_vocab_size):
         super(modelTransformer, self).__init__()
         self.src_word_embedding = nn.Embedding(src_vocab_size, embedding_size),
+        self.positional_encoding = PositionalEncoding(d_model=embedding_size),
         self.transformer = nn.Transformer(d_model = embedding_size),
         self.fc_out = nn.Linear(embedding_size, trg_vocab_size),
 
     def forward(self, x):
         x = self.src_word_embedding(x)
-        # ...
+        x = self.positional_encoding(x)
+        x = self.transformer(x)
+        x = self.fc_out(x)
         return x
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p = dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
 
 class UtilityRNN():
     def getUniqueWords(text):
@@ -60,19 +83,42 @@ class UtilityRNN():
         if math.floor(len(text.split())/batch_size) < len(batches): batches = batches[0:-1]
         return [[word2index[word] for word in batch] for batch in batches]
 
+def TrainingLoop(num_epochs, model, optimizer, text, batches, trg_vocab_size):
+    for epoch in range(num_epochs):
+        for numBatch in range(len(batches)):
+            input = batches[numBatch]
+            output = model(input)
+            loss = training_step(input, output, text, trg_vocab_size) 
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            print('Epoch:{}, Batch number: {}'.format(epoch, numBatch))
+
+def training_step(input, output, text, trg_vocab_size):
+    index = text.find(input)
+    exp_output = np.zeros(len(trg_vocab_size))
+    exp_output[index+1] = 1
+    criterion = nn.CrossEntropyLoss()
+    loss = criterion(output, exp_output) 
+    return loss
 
 def main():
 
-    with open ("textfiles/data.txt", "r") as text:
-        text=text.read().replace('\n', '')
+    #with open ("textfiles/data.txt", "r") as text:
+    #    text=text.read().replace('\n', '')
+    
+    text = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
     uniqueWords = UtilityRNN.getUniqueWords(text)
     word2index = UtilityRNN.assignIndex(uniqueWords)
+    batches = UtilityRNN.encodeText(text, word2index, 2)
 
-    embedding_size = 512
+    embedding_size = 20
     src_vocab_size = len(word2index)
-    trg_vocab_size = 10 # ?
-
+    trg_vocab_size = len(word2index)
+    num_epochs = 20 
     model = modelTransformer(src_vocab_size, embedding_size, trg_vocab_size)
+    optimizer = torch.optim.adam(model.parameters(), lr = 0.005)
+    TrainingLoop(num_epochs, model, optimizer, text, batches, trg_vocab_size)
 
 if __name__ == '__main__':
     main()
