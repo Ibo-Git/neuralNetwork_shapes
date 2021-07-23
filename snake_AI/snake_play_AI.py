94c5aaf277aaf51bb6ea76d720360ac557bed85b
Ibo-Git <ibo1997.it+git@gmail.com>
2021-07-23 21:04:43 +0200

    added 2 frame input

--- snake_AI/snake_play_AI.py
+++ snake_AI/snake_play_AI.py
@@ -8,23 +8,27 @@ def play():
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     game = SnakeGame(width=4, height=4, show_UI=1, gamespeed=5)
-    agent = SnakeAgent(game, lr=0.001, batch_size=64, replay_memory_size=10000, target_update=10, device=device)
+    agent = SnakeAgent(game, lr=0.01, batch_size=64, replay_memory_size=10000, target_update=10, device=device)
+    #game = SnakeGame(width=10, height=10, show_UI=1, gamespeed=5)
+    #agent = SnakeAgent(game, lr=0.00035, batch_size=128, replay_memory_size=10000, target_update=10, device=device)
+    agent.policy_net.load('snake_model_4x4_almost_perfect', agent.policy_net, device)
 
-    agent.policy_net.load('snake_model', agent.policy_net, device)
-
-
+    previous_state = agent.get_state()
     while True:
         # Initialize the environment and state
         game.restart()
         done = 0
         while not done:
             # Get current state
-            state = agent.get_state()
+            current_state = agent.get_state()
+            state_memory = torch.cat([previous_state, current_state], 0)             
+            previous_state = current_state 
+
             # Select and perform an action
-            action = agent.select_action(state, mode='eval')
+            action = agent.select_action(state_memory, mode='eval')
             _, done, score = game.play_step(action.item())
 
-            # if game is done store record, plot and log results
+            # print score
             if done:
                 print(score)             
                 break
