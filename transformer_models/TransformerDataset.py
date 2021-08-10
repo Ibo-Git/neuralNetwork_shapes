591d0e1330e6f1ed9e0cc5dd8a5ddef9279258d5
Ibo-Git <ibo1997.it+git@gmail.com>
2021-08-10 15:12:07 +0200

    changed collate_fn to output minibatches if available

--- transformer_models/TransformerDataset.py
+++ transformer_models/TransformerDataset.py
@@ -11,20 +11,11 @@ class TokenIDX():
 
 
 class TransformerDataset(Dataset):
-    def __init__(self, decoder_input, expected_output, batch_size, minibatch_size=None):
+    def __init__(self, decoder_input, expected_output, batch_size):
         # expects decoder_input and expected_output to be a list of shape [N, sequence]
         # cuts off end of list if it does not fit into batch_size
-        self.decoder_input = self.transform_to_tensor(decoder_input[0:len(decoder_input) // batch_size * batch_size])
+        self.decoder_input = self.insert_SOS(decoder_input[0:len(decoder_input) // batch_size * batch_size])
         self.expected_output = self.insert_EOS(expected_output[0:len(expected_output) // batch_size * batch_size])
-
-        # define shape
-        if minibatch_size is not None:
-            self.reshape_size = (batch_size // minibatch_size, minibatch_size, -1)
-            self.reshape_flat = (batch_size // minibatch_size, -1)
-        else:
-            self.reshape_size = (batch_size, -1)
-            self.reshape_flat = (-1)
-
 
     def __len__(self):
         return len(self.expected_output)
@@ -38,30 +29,26 @@ class TransformerDataset(Dataset):
 
     def insert_EOS(self, dataset):
         # appends EOS to dataset consiting of sequences
-        return [torch.cat((torch.tensor(sequence), torch.tensor(TokenIDX.EOS_IDX))) for sequence in dataset]
+        return [torch.cat((torch.tensor(sequence), torch.tensor([TokenIDX.EOS_IDX]))) for sequence in dataset]
 
 
     def insert_SOS(self, dataset):
-        # insert SOS in dataset consiting of sequences
-        return [torch.cat((torch.tensor(TokenIDX.SOS_IDX), torch.tensor(sequence))) for sequence in dataset]
+        # insert SOS in dataset consiting of 
+        return [torch.cat((torch.tensor([TokenIDX.SOS_IDX]), torch.tensor(sequence))) for sequence in dataset]
+
     
     def transform_to_tensor(self, dataset):
         # transform to tensor without insert EOS or SOS
         return [torch.tensor(sequence) for sequence in dataset]
 
 
-
-    def collate_fn(self, batch):
-
+    @staticmethod
+    def collate_fn(batch):
         decoder_input, expected_output = zip(*batch)
-
         # pad batch
         decoder_input = pad_sequence(decoder_input, batch_first=True, padding_value = TokenIDX.PAD_IDX)
         expected_output = pad_sequence(expected_output, batch_first=True, padding_value = TokenIDX.PAD_IDX)
-
         # reshape
-        decoder_input = decoder_input.reshape(self.reshape_size)        
-        expected_output = expected_output.reshape(self.reshape_size)
-        expected_output_flat = expected_output.reshape(self.reshape_flat)
+        expected_output_flat = expected_output.reshape(-1)
 
         return  decoder_input, expected_output, expected_output_flat
